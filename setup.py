7e1887675dcc64230d71cf7d46918b1e42870e77
Jakob <Jakob@Jakobs-MacBook-Air.local>
2018-05-17 09:28:59 +0200

    Finished creating and linking database

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/local/python3
+
+from setuptools import setup
+
+setup(
+	name='flaskr',
+	packages=['flaskr'],
+	include_package_data=True,
+	install_requires=[
+		'flask',
+	],
+)
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/Jakob/Coding/flaskfiddle/venv"
+set -gx VIRTUAL_ENV "/Users/Jakob/Coding/flaskr/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
